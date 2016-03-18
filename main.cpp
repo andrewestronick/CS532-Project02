7cae5bb6db00246a0c7dd3d3d21d4343eabee571
andrewestronick <andy.estronick@hotmail.com>
2016-03-17 21:32:40 -0700

    Update input file path

--- main.cpp
+++ main.cpp
@@ -3,19 +3,21 @@
 // Computer Science 532 - Advanced Data Structures
 
 #include <iostream>
+#include <string>
 #include "huffman.h"
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    huffman h("F:/Andrew/_code/CS532.local/Project02/input1.txt");
+    const string path = "F:/Andrew/_code/CS532/CS532-Project02/";
+    huffman h(path + "input1.txt");
     h.getCode(h.buildTree(), "");
     h.printResults();
 
     cout << "\n\n------------------------------------------------------------------\n\n";
 
-    huffman i("F:/Andrew/_code/CS532.local/Project02/input2.txt");
+    huffman i(path + "input2.txt");
     i.getCode(i.buildTree(), "");
     i.printResults();
 
